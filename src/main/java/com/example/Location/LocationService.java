34a25ad37a1ffa24d52cf073fb9ebbdbbef57463
benoit.charroux <benoit.charroux@EFREI89124M2-P.campus.villejuif>
2018-10-25 17:10:21 +0200

    get car

--- src/main/java/com/example/Location/LocationService.java
+++ src/main/java/com/example/Location/LocationService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -25,6 +26,17 @@ public class LocationService {
 	@ResponseStatus(HttpStatus.OK) 
 	public ArrayList<Car> listCar() {
 		return cars;
+	}
+
+	@RequestMapping(value="/cars/{plaque}", method=RequestMethod.GET) 
+	@ResponseStatus(HttpStatus.OK) 
+	public Car car(@PathVariable("plaque") String plateNumber ) {
+		for(int i=0; i<cars.size(); i++){
+			if(cars.get(i).getPlateNumber().equals(plateNumber)){
+				return cars.get(i);
+			} 
+		}
+		return null;
 	}
 	
 	@RequestMapping(value="/car", method=RequestMethod.GET) 
